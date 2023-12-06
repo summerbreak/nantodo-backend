2707740d838e18e881359641c27ba9353b8327c4
211250167 <211250167@smail.nju.edu.cn>
2023-12-06 16:24:07 +0800

    complete Homework

--- src/main/java/com/example/nantodo_backend/document/Homework.java
+++ src/main/java/com/example/nantodo_backend/document/Homework.java
@@ -4,9 +4,17 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.*;
+
 @Document(collection = "homework")
 @Data
 public class Homework {
     @Id
     private String id;
+    private String title;
+    private String content;
+    private Date releaseTime;
+    private Date deadline;
+    private List<String> doneGroups;
+    private String courseId;
 }
