ebe9ad3be6fcdd70ba621a68e2dd54f9cce1f9e1
211250167 <211250167@smail.nju.edu.cn>
2023-12-06 20:20:59 +0800

    update Group

--- src/main/java/com/example/nantodo_backend/document/Group.java
+++ src/main/java/com/example/nantodo_backend/document/Group.java
@@ -14,6 +14,7 @@ public class Group {
     private String name;
     private String leaderId;
     private String organName;
+    private String description;
     private List<String> members;
     private List<String> tasks;
 }
